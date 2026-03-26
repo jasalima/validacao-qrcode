d83228601464a43a7a4555177e1b3e156865efd0
Jaasiel Lima <65735768+jasalima@users.noreply.github.com>
2026-03-26 17:55:54 -0300

    Update fetch URL in validar.js

--- api/validar.js
+++ api/validar.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       });
     }
 
-    const response = await fetch("COLE_SUA_URL_DO_FLOW_AQUI", {
+    const response = await fetch("https://default456213cf707347c6bf4941b654ad44.9b.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/c170f3d2665d4f359c49ebed0f2596fc/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Z4FD88F15fMH5TSoVik5hDZ-6ROONamcSgjKV32OU3Q", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
